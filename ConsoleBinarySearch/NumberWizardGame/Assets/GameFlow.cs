3673321f71f916908f064ddb5025b51ac231c8c2
Jack Robie Jereza <jackjereza.08@gmail.com>
2018-06-19 18:29:43 +0800

    Console Binary Search

---
+++ ConsoleBinarySearch/NumberWizardGame/Assets/GameFlow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlow : MonoBehaviour {
+
+	int MinimumNumber = 0;
+	int MaximumNumber = 100;
+	int CurrentNumber;
+	int NumberBefore;
+	// Use this for initialization
+	
+	void Start () 
+	{
+		Debug.Log("Welcome");
+		Debug.Log("Please Pick A Number");
+		Debug.Log("The Minimum number is:" + MinimumNumber);
+		Debug.Log("The Maximum number is:" + MaximumNumber);
+		CurrentNumber = MinimumNumber + MaximumNumber / 2;
+		NumberBefore = CurrentNumber;
+		Debug.Log("Is your number is:" + CurrentNumber);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown("up"))
+        {
+        	MinimumNumber = CurrentNumber;
+        	CurrentNumber = (CurrentNumber + MaximumNumber) / 2;
+            Debug.Log("Is your number is:" + CurrentNumber);
+        }
+        if (Input.GetKeyDown("down"))
+        {        	
+			MaximumNumber = CurrentNumber;
+            CurrentNumber = (MinimumNumber + CurrentNumber) / 2;          
+            Debug.Log("Is your number is:" + CurrentNumber);
+        }
+	}
+}
