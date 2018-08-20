6b25ff1a392f808f21e14695ba481e9b77199df8
Jack Robie Jereza <jackjereza.08@gmail.com>
2018-08-20 08:24:11 +0800

    First Prototype

---
+++ Save Meh/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject Player;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - Player.transform.position;	//Act as the camera is the child of the player object.
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = Player.transform.position + offset;
+	}
+}
---
+++ Save Meh/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private float MoveHorizontal;
+	private float MoveVertical;
+	private Rigidbody2D rigid;
+	private Vector2 Movement;
+	// Use this for initialization
+	void Start () 
+	{
+		rigid = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate () 
+	{
+		MoveHorizontal = Input.GetAxisRaw("Horizontal");
+		MoveVertical = Input.GetAxisRaw("Vertical");
+		Movement = new Vector2(MoveHorizontal,MoveVertical);
+		rigid.AddForce(Movement * speed);
+	}
+}
